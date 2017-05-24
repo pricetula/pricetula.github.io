65d155c22554bbe42c074a68be7087c03f820eb9
pricetula <pricetula@gmail.com>
2017-05-24 15:35:57 +0300

    newer again

--- js/init.js
+++ js/init.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-
   $(".button-collapse").sideNav();
 
   $('.parallax').parallax();
 
   AOS.init();
-
 
   $('.dropdown-button').dropdown({
       constrainWidth: true, // Does not change width of dropdown to that of the activator
@@ -13,5 +11,19 @@ $(document).ready(function(){
     }
   );
 
+  $('a').on('click', function (event) {
+    if (this.hash !== '#' && this.hash !== '') {
+      event.preventDefault();
 
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {scrollTop: $(hash).offset().top},
+        800,
+        () => {
+          window.location.hash = hash;
+        },
+      );
+    }
+  });
 });
